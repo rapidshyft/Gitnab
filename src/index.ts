58d26e211611f626454dc9f495632b8fbc546d11
Lwazi Ncube <lwazincubex@gmail.com>
2024-12-06 00:14:09 +0200

    .

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,6 @@
  * - Open a browser tab at http://localhost:8787/ to see your worker in action
  * - Run `npm run deploy` to publish your worker
  *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
